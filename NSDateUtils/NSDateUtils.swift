3a6b38e5a540ea5968d829e30f6452102df8db54
Carlos Alcala <carlos.alcala@icloud.com>
2016-06-04 16:43:38 -0400

    Publicity

--- NSDateUtils/NSDateUtils.swift
+++ NSDateUtils/NSDateUtils.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension NSDate {
     
-    func convertToStringUsingFormat(format: String)->String {
+    public func convertToStringUsingFormat(format: String)->String {
         
         let formatter = NSDateFormatter()
         formatter.dateFormat = format
@@ -18,15 +18,15 @@ extension NSDate {
         return date
     }
     
-    func convertToLongString()->String {
+    public func convertToLongString()->String {
         return convertToStringUsingFormat("EE, LLL d yyyy, HH:mm")
     }
     
-    func convertToString()->String {
+    public func convertToString()->String {
         return convertToStringUsingFormat("yyyy-MM-dd")
     }
     
-    func dateFromString(dateString: String, format: String? = nil) -> NSDate{
+    public func dateFromString(dateString: String, format: String? = nil) -> NSDate{
     
         let formatter = NSDateFormatter()
         if format != nil {
